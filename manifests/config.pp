eba5d513350250f8600baabad51257997acd19a9
Andrew Otto <acotto@gmail.com>
2012-12-05 15:00:40 -0500

    I changed my mind, will force user of kafka::config class to ensure directory exists.

--- manifests/config.pp
+++ manifests/config.pp
@@ -18,13 +18,4 @@ class kafka::config(
 	file { "/etc/kafka/consumer.properties":
 		content => template("kafka/consumer.properties.erb"),
 	}
-
-	# ensure that Kafka log directory exists
-	$log_directory = inline_template("<%= File.dirname(kafka_log_file) %>")
-	file { $log_directory:
-		ensure => "directory",
-		mode   => 0755,
-		owner  => "kafka",
-		group  => "kafka",
-	}
 }
